83065ee0b2bd3e0b9f60015ab6e5459ac2681cc2
ejuer <ejuer_z@163.com>
2026-07-21 14:10:35 +0800

    fix: add BrowserRouter basename for /pixelweb subpath

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/pixelweb">
       <App />
     </BrowserRouter>
   </StrictMode>,
